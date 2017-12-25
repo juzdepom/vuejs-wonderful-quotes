b4330d6b33886acbbf4fa842fa1e545be8f84ea0
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-25 02:48:03 -0800

    pass data with slots and props.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="container">
-
+      <quote-grid :quotes="quotes"></quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
         data: function(){
           return {
             quotes: [
               'This is my default quote.',
+              'This is another quote.'
             ],
             maxQuotes: 10,
           }
+        },
+        components: {
+          QuoteGrid,
         }
     }
 </script>
