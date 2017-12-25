e8e7c77005bc65379984bed81cf5e8d70a6e2e74
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-25 02:31:37 -0800

    initialized application.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-    
+
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function(){
+          return {
+            quotes: [
+              'This is my default quote.',
+            ],
+            maxQuotes: 10,
+          }
+        }
     }
 </script>
 
